f28bafef5814e91dda53345e936bb49dff51fdba
JakubGlisz <31781576+JakubGlisz@users.noreply.github.com>
2022-05-07 16:57:34 +0200

    Added model with SVC as a weak learner

--- load.py
+++ load.py
@@ -1,6 +1,12 @@
 from mlxtend.data import loadlocal_mnist
 from matplotlib import pyplot
 from sklearn.model_selection import train_test_split
+# Load libraries
+from sklearn.ensemble import AdaBoostClassifier
+# Import Support Vector Classifier
+from sklearn.svm import SVC
+#Import scikit-learn metrics module for accuracy calculation
+from sklearn import metrics
 import numpy as np
 
 GRAY_THRESHOLD = 120
@@ -38,13 +44,24 @@ for i in range(SET_SIZE):
     Images[i] = remove_gray(Images[i])
 
 # Spliting choosen part of a data into train and test set
-Train_Images, Test_Images, Train_Labels, Test_Labels = train_test_split(Images, Labels, test_size=0.2, random_state=42)
+Train_Images, Test_Images, Train_Labels, Test_Labels = train_test_split(Images, Labels, test_size=0.2, random_state=32)
+
+# Defining a weak learner
+svc = SVC(probability=True, kernel='linear')
+
+# Creating and training a classifier
+classifier = AdaBoostClassifier(n_estimators=30, base_estimator=svc, learning_rate=1)
+model = classifier.fit(Train_Images, Train_Labels)
+
+y_pred = model.predict(Test_Images)
+
+print("Accuracy:", metrics.accuracy_score(Test_Labels, y_pred))
 
 # Plotting few images
 tmp = []
-for i in range(9):  
+for i in range(9):
+    print(f'Label: {Test_Labels[i]} | Prediction: {y_pred[i]}')
     pyplot.subplot(330 + 1 + i)
-    tmp = np.reshape(Train_Images[i], (IMG_SIZE, IMG_SIZE))
+    tmp = np.reshape(Test_Images[i], (IMG_SIZE, IMG_SIZE))
     pyplot.imshow(tmp)
-print(tmp)
 pyplot.show()
