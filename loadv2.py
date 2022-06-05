b93bfd7daf4a1f0c749a3f15dcd54bf63ebc3c04
Lila <liczaniecka@gmail.com>
2022-06-05 18:10:17 +0200

    Time measurement

--- loadv2.py
+++ loadv2.py
@@ -8,6 +8,7 @@ from sklearn.svm import SVC
 #Import scikit-learn metrics module for accuracy calculation
 from sklearn import metrics
 import numpy as np
+import time
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
@@ -22,11 +23,13 @@ TRAIN_SET_SIZE = 15000
 TEST_SET_SIZE = 3000
 SET_SIZE = TRAIN_SET_SIZE + TEST_SET_SIZE
 
+
 def load_data():
     Images, Labels = loadlocal_mnist(
             images_path='train-images.idx3-ubyte', 
             labels_path='train-labels.idx1-ubyte')
     return Images, Labels
+
 
 def remove_gray(Image):
     for i in range(784):
@@ -35,6 +38,30 @@ def remove_gray(Image):
         else:
             Image[i] = 255
     return Image
+
+
+def create_pie_chart(Test_Labels, y_pred):
+    status_list = []
+
+    for i in range(len(Test_Labels)):
+        if y_pred[i] == Test_Labels[i]:
+            status_list.append('Success')
+        else:
+            status_list.append('Fail')
+
+    proportion = [status_list.count('Success'), status_list.count('Fail')]
+
+    fig1, ax = pyplot.subplots()
+    patches, text, auto = ax.pie(proportion, autopct='%1.1f%%', colors=['#43C456', '#FF6565'],
+                                 wedgeprops={'linewidth': 1, 'edgecolor': 'white'},
+                                 pctdistance=1.15, startangle=160)
+    circle = pyplot.Circle((0, 0), 0.5, color='white')
+    pyplot.gcf().gca().add_artist(circle)
+    pyplot.legend(patches, ['Success', 'Fail'], loc='upper right', fontsize='xx-small', framealpha=0.4)
+
+    pyplot.show()
+    pyplot.savefig('Piechart')
+
 
 # Loading all data
 ImagesAll, LabelsAll = load_data()
@@ -58,8 +85,12 @@ rfc = RandomForestClassifier(n_estimators=40, random_state=1, criterion='gini') 
                 # for smaller samples entropy works better
 gnb = GaussianNB() # 64.4%
 
+start = time.time()
+
 classifier = AdaBoostClassifier(n_estimators=40, base_estimator=rfc, learning_rate=1.2)
 model = classifier.fit(Train_Images, Train_Labels)
+
+end = time.time()
 
 y_pred = model.predict(Test_Images)
 
@@ -72,29 +103,8 @@ for i in range(9):
     pyplot.subplot(330 + 1 + i)
     tmp = np.reshape(Test_Images[i], (IMG_SIZE, IMG_SIZE))
     pyplot.imshow(tmp)
+
+print(f'Time: {end-start}')
 pyplot.show()
 
-#AI Project
-# Pie no1
-#results_df = pd.DataFrame({'Labels': Test_Labels,
-#                           'Prediction': y_pred})
-#
-#s_list = []
-#
-#for i in range(9):
-#    if y_pred[i] == Test_Labels[i]:
-#        s_list.append('Success')
-#    else:
-#        s_list.append('Fail')
-#
-#results_df['Outcome'] = s_list
-#results_df.plot.pie(y=s_list)
-#
-## Pie no2
-#fig1, ax = pyplot.subplots()
-#patches, text, auto = ax.pie(s_list, autopct='%1.1f%%', colors=['red','green'],
-#                             wedgeprops={'linewidth': 1, 'edgecolor': 'white'},
-#                             pctdistance=1.15, startangle=160)
-#circle = pyplot.Circle((0, 0), 0.5, color='white')
-#pyplot.gcf().gca().add_artist(circle)
-#pyplot.legend(patches, ['Success', 'Fail'], loc='upper right', fontsize='xx-small', framealpha=0.4)
+# create_pie_chart(Test_Labels, y_pred)
